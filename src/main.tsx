934eb902af5d6bd975ccdae12ca5a89e4374d3ef
Omar Soriano <osoriano2@gmail.com>
2025-01-19 17:35:08 -0800

    Add absolute imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+
+import "src/index.css";
+import App from "src/App.tsx";
 
 createRoot(document.getElementById("root") as Element).render(
   <StrictMode>
