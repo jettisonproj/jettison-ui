7488a823749f80e63c1c8ac4f92714cc1afa46ab
Omar Soriano <osoriano2@gmail.com>
2025-01-20 13:57:24 -0800

    Use css modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "src/index.css";
+import "src/main.css";
 import App from "src/App.tsx";
 
 createRoot(document.getElementById("root") as Element).render(
