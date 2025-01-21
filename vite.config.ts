4d91409b450d87f91a2cbb719b39cd8438837b11
Omar Soriano <osoriano2@gmail.com>
2025-01-20 19:51:15 -0800

    run eslint

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-// eslint-disable-next-line
+// eslint-disable-next-line no-restricted-syntax
 export default defineConfig({
   plugins: [react()],
   resolve: {
