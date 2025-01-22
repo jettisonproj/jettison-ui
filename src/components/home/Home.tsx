10d5be26986a9ba3c19170546af71a904f2ac698
Omar Soriano <osoriano2@gmail.com>
2025-01-22 07:51:37 -0800

    Home page skeleton

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,13 +1,60 @@
-import { Content } from "src/components/content/Content.tsx"
-import { Header } from "src/components/header/Header.tsx"
+import { Link } from "react-router";
+
+import { recentFlows, stats } from "src/data/data.ts";
+import { Content } from "src/components/content/Content.tsx";
+import { Header } from "src/components/header/Header.tsx";
+import styles from "src/components/home/Home.module.css";
 
 function Home() {
   return (
     <>
       <Header />
-      <Content title='Home'>
-        <p>This is the HOME page</p>
+      <Content title="Home">
+        <Overview />
+        <RecentFlows />
       </Content>
+    </>
+  );
+}
+
+function Overview() {
+  return (
+    <>
+      <h2>Overview</h2>
+      <div className={styles.overviewContainer}>
+        <p>
+          <label className={styles.overviewLabel}>Namespaces</label>{" "}
+          {stats.namespaces}
+        </p>
+        <p>
+          <label className={styles.overviewLabel}>Flows</label> {stats.flows}
+        </p>
+        <p>
+          <label className={styles.overviewLabel}>Triggers</label>{" "}
+          {stats.triggers}
+        </p>
+        <p>
+          <label className={styles.overviewLabel}>Steps</label> {stats.steps}
+        </p>
+        <Link to="flows">
+          See All Namespaces <i className="fa fa-lg fa-angle-right" />
+        </Link>
+      </div>
+    </>
+  );
+}
+
+function RecentFlows() {
+  return (
+    <>
+      <h2 className={styles.sectionTitle}>Recent Flows</h2>
+      <ul className={styles.recentFlowsContainer}>
+        {recentFlows.map((recentFlow) => (
+          <li key={recentFlow}>
+            <Link to={`flows/${recentFlow}`}>{recentFlow}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
---
+++ src/data/data.ts
@@ -0,0 +1,10 @@
+const stats = {
+  namespaces: 5,
+  flows: 5,
+  triggers: 5,
+  steps: 5,
+};
+
+const recentFlows = ["rollouts-demo/github-push", "rollouts-demo/github-pr"];
+
+export { recentFlows, stats };
