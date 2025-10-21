6bd70b32b5c3765ee34a396a8ff784cd89d99a9c
Omar Soriano <osoriano2@gmail.com>
2025-10-20 18:37:51 -0700

    export vars

--- src/utils/flowUtil.ts
+++ src/utils/flowUtil.ts
@@ -1,8 +1,11 @@
+const PR_DISPLAY_NAME = "PR";
+const PUSH_DISPLAY_NAME = "PUSH";
+
 function getTriggerDisplayName(isPrFlow: boolean) {
   if (isPrFlow) {
-    return "PR";
+    return PR_DISPLAY_NAME;
   }
-  return "PUSH";
+  return PUSH_DISPLAY_NAME;
 }
 
-export { getTriggerDisplayName };
+export { getTriggerDisplayName, PR_DISPLAY_NAME, PUSH_DISPLAY_NAME };
