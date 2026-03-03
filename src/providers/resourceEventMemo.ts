a2f3e3ee3727e9aaad4ffd9404afc8d2efc28301
Omar Soriano <osoriano2@gmail.com>
2026-03-02 16:23:35 -0800

    update sorted node ordering
    trigger nodes come first

--- src/providers/resourceEventMemo.ts
+++ src/providers/resourceEventMemo.ts
@@ -5,7 +5,7 @@ import type {
   WorkflowMemoStatusNode,
 } from "src/data/types/workflowTypes.ts";
 import { formatDuration } from "src/utils/dateUtil.ts";
-import { isMemoizedNode } from "src/utils/workflowUtil.ts";
+import { isMemoizedNode, TRIGGER_NODE_NAME } from "src/utils/workflowUtil.ts";
 
 function memoizeFlow(flow: Flow) {
   const trigger = getFlowTrigger(flow);
@@ -99,7 +99,7 @@ function memoizeWorkflow(workflow: Workflow) {
   }
 
   // Memoize the sorted nodes by startedAt
-  sortedNodes.sort((a, b) => a.startedAt.getTime() - b.startedAt.getTime());
+  sortedNodes.sort(workflowMemoNodeCompareFn);
 
   // Memoize the parameter List to a Map
   const { parameters } = workflow.spec.arguments;
@@ -127,6 +127,21 @@ function memoizeWorkflow(workflow: Workflow) {
       finishedAtDate.getTime() - startedAt.getTime(),
     );
   }
+}
+
+function workflowMemoNodeCompareFn(
+  a: WorkflowMemoStatusNode,
+  b: WorkflowMemoStatusNode,
+) {
+  // Ensure trigger nodes come first
+  if (a.displayName === TRIGGER_NODE_NAME) {
+    return -1;
+  }
+  if (b.displayName === TRIGGER_NODE_NAME) {
+    return 1;
+  }
+  // Then, order by time
+  return a.startedAt.getTime() - b.startedAt.getTime();
 }
 
 class MemoizeError extends Error {
