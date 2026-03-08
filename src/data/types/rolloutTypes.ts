72d1786952dcfa684c8740bbe13dd531c81e3441
Omar Soriano <osoriano2@gmail.com>
2026-03-08 14:47:04 -0700

    update health condition

--- src/data/types/rolloutTypes.ts
+++ src/data/types/rolloutTypes.ts
@@ -9,7 +9,20 @@ interface Rollout extends NamespacedResource {
 }
 
 interface RolloutStatus {
-  phase: string;
+  phase: RolloutPhase;
+}
+
+// SOT: https://pkg.go.dev/github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1#RolloutPhase
+enum RolloutPhase {
+  // RolloutPhaseHealthy indicates a rollout is healthy
+  Healthy = "Healthy",
+  // RolloutPhaseDegraded indicates a rollout is degraded (e.g. pod unavailability, misconfiguration)
+  Degraded = "Degraded",
+  // RolloutPhaseProgressing indicates a rollout is not yet healthy but still making progress towards a healthy state
+  Progressing = "Progressing",
+  // RolloutPhasePaused indicates a rollout is not yet healthy and will not make progress until unpaused
+  Paused = "Paused",
 }
 
 export type { Rollout };
+export { RolloutPhase };
