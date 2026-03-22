705ad64741bd7c3a6c238584635fd87d35d78a98
Omar Soriano <osoriano2@gmail.com>
2026-03-22 13:41:24 -0700

    Add status badge

---
+++ src/components/flow/history/FlowHistoryStatusBadge.tsx
@@ -0,0 +1,97 @@
+import styles from "src/components/flow/history/FlowHistoryStatusBadge.module.css";
+import { LoadIcon } from "src/components/icons/LoadIcon.tsx";
+import { WorkflowPhase } from "src/data/types/workflowTypes.ts";
+import type { Workflow } from "src/data/types/workflowTypes.ts";
+import {
+  getWorkflowRepo,
+  getWorkflowRevision,
+  getWorkflowRevisionNumber,
+} from "src/utils/workflowUtil.ts";
+import {
+  getDisplayCommit,
+  getRepoCommitLink,
+  getRepoPrLink,
+} from "src/utils/gitUtil.ts";
+
+interface FlowHistoryStatusBadgeProps {
+  isPrFlow: boolean;
+  workflow: Workflow;
+}
+function FlowHistoryStatusBadge({
+  isPrFlow,
+  workflow,
+}: FlowHistoryStatusBadgeProps) {
+  const { title, href } = getStatusLinkData(isPrFlow, workflow);
+  return (
+    <a
+      className={styles.historyStatusBadge}
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <FlowHistoryStatusIcon workflow={workflow} />
+      <span className={styles.historyCommitText}>{title}</span>
+    </a>
+  );
+}
+
+interface FlowHistoryStatusIconProps {
+  workflow: Workflow;
+}
+
+function FlowHistoryStatusIcon({ workflow }: FlowHistoryStatusIconProps) {
+  const { phase } = workflow.status;
+  switch (phase) {
+    case WorkflowPhase.Succeeded:
+      return <i className={`nf nf-fa-circle_check ${styles.successIcon}`} />;
+    case WorkflowPhase.Error:
+      return <i className={`nf nf-md-cancel ${styles.dangerIcon}`} />;
+    case WorkflowPhase.Failed:
+      return <i className={`nf nf-fa-circle_xmark ${styles.dangerIcon}`} />;
+    case WorkflowPhase.Running:
+      return <LoadIcon className={styles.loadingIcon} />;
+    case WorkflowPhase.Pending:
+      return <i className={`nf nf-fa-clock ${styles.badgeIcon}`} />;
+    case WorkflowPhase.Unknown:
+      return <i className={`nf nf-fa-question_circle_o ${styles.badgeIcon}`} />;
+    default:
+      phase satisfies never;
+      console.log("unknown workflow phase:");
+      console.log(phase);
+      throw new FlowHistoryStatusBadgeError("unknown workflow phase");
+  }
+}
+
+function getStatusLinkData(isPrFlow: boolean, workflow: Workflow) {
+  const { parameterMap } = workflow.memo;
+
+  if (isPrFlow) {
+    const repoUrl = getWorkflowRepo(parameterMap);
+    const prNumber = getWorkflowRevisionNumber(parameterMap);
+    const prLink = getRepoPrLink(repoUrl, prNumber);
+
+    return {
+      title: `#${prNumber}`,
+      href: prLink,
+    };
+  }
+
+  const commit = getWorkflowRevision(parameterMap);
+  const repoUrl = getWorkflowRepo(parameterMap);
+  const displayCommit = getDisplayCommit(commit);
+  const commitLink = getRepoCommitLink(repoUrl, commit);
+
+  return {
+    title: displayCommit,
+    href: commitLink,
+  };
+}
+
+class FlowHistoryStatusBadgeError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export { FlowHistoryStatusBadge };
