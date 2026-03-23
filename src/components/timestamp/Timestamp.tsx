638c113b928db202c7837184e5fd8891e22e2c27
Omar Soriano <osoriano2@gmail.com>
2026-03-23 02:37:13 -0700

    variable tick for timestamp

--- src/components/timestamp/Timestamp.tsx
+++ src/components/timestamp/Timestamp.tsx
@@ -1,27 +1,11 @@
-import { useContext } from "react";
+import { useEffect, useState, useRef, useCallback, useContext } from "react";
 
 import { TimestampFormat } from "src/localState.ts";
-import { formatTimestamp } from "src/utils/dateUtil.ts";
+import { formatTimestamp, getNextTickSeconds } from "src/utils/dateUtil.ts";
 import {
   TimestampFormatContext,
   SetTimestampFormatContext,
 } from "src/providers/provider.tsx";
-
-function getDisplayTimestamp(d: Date, timestampFormat: TimestampFormat) {
-  switch (timestampFormat) {
-    case TimestampFormat.Relative:
-      return formatTimestamp(Math.trunc((Date.now() - d.getTime()) / 1000));
-    case TimestampFormat.Locale:
-      return d.toLocaleString();
-    case TimestampFormat.Iso:
-      return d.toISOString();
-    default:
-      timestampFormat satisfies never;
-      console.log("unknown timestamp format to display");
-      console.log(timestampFormat);
-      throw new TimestampError("unknown timestamp format to display");
-  }
-}
 
 function getNextTimestampFormat(timestampFormat: TimestampFormat) {
   switch (timestampFormat) {
@@ -49,7 +33,24 @@ function Timestamp({ date, className }: TimestampProps) {
   const timestampFormat = useContext(TimestampFormatContext);
   const setTimestampFormat = useContext(SetTimestampFormatContext);
 
-  const displayTimestamp = getDisplayTimestamp(date, timestampFormat);
+  let formattedTimestamp;
+  switch (timestampFormat) {
+    case TimestampFormat.Relative:
+      formattedTimestamp = <RelativeTimestamp date={date} />;
+      break;
+    case TimestampFormat.Locale:
+      formattedTimestamp = date.toLocaleString();
+      break;
+    case TimestampFormat.Iso:
+      formattedTimestamp = date.toISOString();
+      break;
+    default:
+      timestampFormat satisfies never;
+      console.log("unknown timestamp format to display");
+      console.log(timestampFormat);
+      throw new TimestampError("unknown timestamp format to display");
+  }
+
   return (
     <span
       className={className}
@@ -57,9 +58,52 @@ function Timestamp({ date, className }: TimestampProps) {
         setTimestampFormat(getNextTimestampFormat);
       }}
     >
-      {displayTimestamp}
+      {formattedTimestamp}
     </span>
   );
+}
+
+interface RelativeTimestampProps {
+  date: Date;
+}
+function RelativeTimestamp({ date }: RelativeTimestampProps) {
+  const [secondsElapsed, setSecondsElapsed] = useState(() =>
+    Math.trunc((Date.now() - date.getTime()) / 1000),
+  );
+
+  /* For this component, the source of truth is secondsElapsedRef,
+   * which will sync its values with secondsElapsed after a timeout */
+  const secondsElapsedRef = useRef<number>(secondsElapsed);
+  const timeoutRef = useRef<number | null>(null);
+
+  const stopWatchTick = useCallback(() => {
+    setSecondsElapsed(secondsElapsedRef.current);
+
+    const nextTickSeconds = getNextTickSeconds(secondsElapsedRef.current);
+    if (nextTickSeconds < 0) {
+      return;
+    }
+    secondsElapsedRef.current += nextTickSeconds;
+    timeoutRef.current = setTimeout(stopWatchTick, nextTickSeconds * 1_000);
+  }, [setSecondsElapsed]);
+
+  useEffect(() => {
+    const nextTickSeconds = getNextTickSeconds(secondsElapsedRef.current);
+    if (nextTickSeconds < 0) {
+      return;
+    }
+
+    secondsElapsedRef.current += nextTickSeconds;
+    timeoutRef.current = setTimeout(stopWatchTick, nextTickSeconds * 1_000);
+
+    return () => {
+      if (timeoutRef.current != null) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [stopWatchTick]);
+
+  return formatTimestamp(secondsElapsed);
 }
 
 class TimestampError extends Error {
