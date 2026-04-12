75462d1c74b38ffa3481b745329689f122cd73c0
Omar <osoriano2@gmail.com>
2026-04-12 16:06:27 -0700

    graph badges (#40)
    * separate badge component
    
    * separate badge css
    
    * live badge
    
    * failing badge
    
    * not found badge
    
    * paused badge
    
    * drift icon
    
    * pr feedback
    
    * more feedback

---
+++ src/components/flow/graph/nodes/steps/FlowGraphArgoCDBadge.tsx
@@ -0,0 +1,62 @@
+import styles from "src/components/flow/graph/nodes/steps/FlowGraphArgoCDBadge.module.css";
+
+interface ArgoCDBadgeProps {
+  title: string | undefined;
+}
+
+function ArgoCDNotFoundBadge() {
+  return (
+    <div className={styles.failingBadge}>
+      <i className={`nf nf-fa-circle_xmark ${styles.notFoundIcon}`} />
+      <div className={styles.notFoundBadgeText}>Resource Not Found</div>
+    </div>
+  );
+}
+
+function ArgoCDFailingBadge({ title }: ArgoCDBadgeProps) {
+  return (
+    <div className={styles.failingBadge} title={title}>
+      <i className={`nf nf-oct-pulse ${styles.pulseIcon}`} />
+      <div className={styles.failingDotIcon}></div>
+      <div className={styles.badgeText}>Failing</div>
+    </div>
+  );
+}
+
+function ArgoCDLiveBadge() {
+  return (
+    <div className={styles.liveBadge}>
+      <i className={`nf nf-oct-pulse ${styles.pulseIcon}`} />
+      <div className={styles.liveDotIcon}></div>
+      <div className={styles.badgeText}>Healthy</div>
+    </div>
+  );
+}
+
+function ArgoCDPausedBadge({ title }: ArgoCDBadgeProps) {
+  return (
+    <div className={styles.pausedBadge} title={title}>
+      <i className={`nf nf-oct-pulse ${styles.pulseIcon}`} />
+      <div className={styles.pausedDotIcon}></div>
+      <div className={styles.badgeText}>Paused</div>
+    </div>
+  );
+}
+
+function ArgoCDDriftBadge({ title }: ArgoCDBadgeProps) {
+  return (
+    <div className={styles.driftBadge} title={title}>
+      <i className={`nf nf-oct-pulse ${styles.pulseIcon}`} />
+      <div className={styles.driftDotIcon}></div>
+      <div className={styles.badgeText}>Resource Drifted</div>
+    </div>
+  );
+}
+
+export {
+  ArgoCDDriftBadge,
+  ArgoCDFailingBadge,
+  ArgoCDLiveBadge,
+  ArgoCDNotFoundBadge,
+  ArgoCDPausedBadge,
+};
