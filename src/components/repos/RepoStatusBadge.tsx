9164e29d38a3da89323f4b80fb180bc5851d7825
Omar Soriano <osoriano2@gmail.com>
2026-04-26 14:09:42 -0700

    add RepoStatusBadge

---
+++ src/components/repos/RepoStatusBadge.tsx
@@ -0,0 +1,80 @@
+import { LoadIcon } from "src/components/icons/LoadIcon.tsx";
+import styles from "src/components/repos/RepoStatusBadge.module.css";
+import type { WorkflowPhase } from "src/data/types/workflowTypes.ts";
+import { WorkflowPhases } from "src/data/types/workflowTypes.ts";
+
+interface RepoStatusBadgeProps {
+  workflowPhase: WorkflowPhase;
+}
+function RepoStatusBadge({ workflowPhase }: RepoStatusBadgeProps) {
+  switch (workflowPhase) {
+    case WorkflowPhases.Succeeded:
+      return (
+        <div className={`${styles.repoStatusBadge} ${styles.repoBadgeSuccess}`}>
+          <div className={styles.repoBadgeText}>
+            <i className={`nf nf-fa-circle_check ${styles.repoBadgeIcon}`} />
+            {workflowPhase}
+          </div>
+        </div>
+      );
+    case WorkflowPhases.Error:
+      return (
+        <div className={`${styles.repoStatusBadge} ${styles.repoBadgeDanger}`}>
+          <div className={styles.repoBadgeText}>
+            <i className={`nf nf-md-cancel ${styles.repoBadgeIcon}`} />
+            {workflowPhase}
+          </div>
+        </div>
+      );
+    case WorkflowPhases.Failed:
+      return (
+        <div className={`${styles.repoStatusBadge} ${styles.repoBadgeDanger}`}>
+          <div className={styles.repoBadgeText}>
+            <i className={`nf nf-fa-circle_xmark ${styles.repoBadgeIcon}`} />
+            {workflowPhase}
+          </div>
+        </div>
+      );
+    case WorkflowPhases.Running:
+      return (
+        <div className={`${styles.repoStatusBadge} ${styles.repoBadgeRunning}`}>
+          <div className={styles.repoBadgeText}>
+            <LoadIcon className={styles.repoBadgeIcon} />
+            {workflowPhase}
+          </div>
+        </div>
+      );
+    case WorkflowPhases.Pending:
+      return (
+        <div className={`${styles.repoStatusBadge} ${styles.repoBadgePending}`}>
+          <div className={styles.repoBadgeText}>
+            <i className={`nf nf-fa-clock ${styles.repoBadgeIcon}`} />
+            {workflowPhase}
+          </div>
+        </div>
+      );
+    case WorkflowPhases.Unknown:
+      return (
+        <div className={`${styles.repoStatusBadge} ${styles.repoBadgePending}`}>
+          <div className={styles.repoBadgeText}>
+            <i className={`nf nf-md-cancel ${styles.repoBadgeIcon}`} />
+            Unknown
+          </div>
+        </div>
+      );
+    default:
+      workflowPhase satisfies never;
+      console.log("unknown repo status workflow phase:");
+      console.log(workflowPhase);
+      throw new RepoStatusBadgeError("unknown phase for workflow");
+  }
+}
+
+class RepoStatusBadgeError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+export { RepoStatusBadge };
