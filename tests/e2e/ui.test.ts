5b93097df7513a03a7bf530c983432db6ba77ae0
Omar Soriano <osoriano2@gmail.com>
2026-05-03 17:52:07 -0700

    fix e2e

--- tests/e2e/ui.test.ts
+++ tests/e2e/ui.test.ts
@@ -8,51 +8,69 @@ const YAML_URL_RE = new RegExp(
 const WORKFLOW_UI_URL_RE = new RegExp(
   `https://argo\\.osoriano\\.com/workflows/${REPO_ORG}/${REPO_NAME}-`,
 );
+const PR_FLOW_RE = /PR Flow$/;
+const VIEW_YAML_RE = /View YAML$/;
+const VIEW_IN_WORKFLOW_UI_RE = /View in Workflow UI$/;
+const SEE_WORKFLOW_DETAILS_RE = /See Details$/;
+
+// Shorthand variable for enabling extact matching
+const exact = true;
 
 test("test", async ({ page }) => {
   await page.goto("/");
 
   // Test Home Page
-  await expect(page.getByRole("heading", { name: "Home" })).toBeVisible();
-  await expect(page.getByRole("heading", { name: "Overview" })).toBeVisible();
   await expect(
-    page.getByRole("heading", { name: "Recent Repos" }),
+    page.getByRole("heading", { name: "Home", exact }),
   ).toBeVisible();
-  await expect(page.getByText("No recent repos found")).toBeVisible();
-  await page.getByRole("link", { name: "See All Repos" }).click();
+  await expect(
+    page.getByRole("heading", { name: "Overview", exact }),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Recent Repos", exact }),
+  ).toBeVisible();
+  await expect(
+    page.getByText("No recent repos found", { exact }),
+  ).toBeVisible();
+  await page.getByRole("link", { name: /^See All Repos/ }).click();
 
   // Test Repos Page
-  await expect(page.getByRole("heading", { name: "Home⧸Repos" })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Home⧸Repos", exact }),
+  ).toBeVisible();
 
-  const jettisonUiRepoLink = page.getByRole("link", { name: "jettison-ui" });
+  const jettisonUiRepoLink = page.getByRole("link", {
+    name: "jettison-ui",
+    exact,
+  });
   await expect(jettisonUiRepoLink).toBeVisible();
   await jettisonUiRepoLink.click();
 
   // Test Push Flow Page
   await expect(
-    page.getByRole("heading", { name: `Home⧸Repos⧸${REPO_NAME}` }),
+    page.getByRole("heading", { name: `Home⧸Repos⧸${REPO_NAME}`, exact }),
   ).toBeVisible();
-  await expect(page.getByRole("link", { name: "Push Flow" })).toBeVisible();
+  await expect(page.getByRole("link", { name: /Push Flow$/ })).toBeVisible();
   const jettisonFlowPushGraphSvg = page.locator("svg");
   await expect(jettisonFlowPushGraphSvg).toHaveCount(1);
   await expect(jettisonFlowPushGraphSvg).toBeVisible();
 
   // Test Push Flow Page - Workflow Execution
   const firstPushWorkflowDetails = page
-    .getByRole("link", { name: "See Details" })
+    .getByRole("link", { name: SEE_WORKFLOW_DETAILS_RE })
     .first();
   await expect(firstPushWorkflowDetails).toBeVisible();
   await firstPushWorkflowDetails.hover();
   await page.getByRole("button").click();
 
   const viewWorkflowPushYamlLink = page.getByRole("link", {
-    name: "View YAML",
+    name: VIEW_YAML_RE,
   });
   await expect(viewWorkflowPushYamlLink).toBeVisible();
   await expect(viewWorkflowPushYamlLink).toHaveAttribute("href", YAML_URL_RE);
 
   const viewPushWorkflowUiLink = page.getByRole("link", {
-    name: "View in Workflow UI",
+    name: VIEW_IN_WORKFLOW_UI_RE,
   });
   await expect(viewPushWorkflowUiLink).toBeVisible();
   await expect(viewPushWorkflowUiLink).toHaveAttribute(
@@ -61,33 +79,35 @@ test("test", async ({ page }) => {
   );
 
   await firstPushWorkflowDetails.click();
-  await expect(page.getByRole("link", { name: "Summary" })).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "summary", exact }),
+  ).toBeVisible();
 
-  await page.getByRole("link", { name: "PR Flow" }).click();
+  await page.getByRole("link", { name: PR_FLOW_RE }).click();
 
   // Test PR Flow Page
   await expect(
-    page.getByRole("heading", { name: `Home⧸Repos⧸${REPO_NAME}` }),
+    page.getByRole("heading", { name: `Home⧸Repos⧸${REPO_NAME}`, exact }),
   ).toBeVisible();
-  await expect(page.getByRole("link", { name: "PR Flow" })).toBeVisible();
+  await expect(page.getByRole("link", { name: PR_FLOW_RE })).toBeVisible();
   const jettisonPrFlowGraphSvg = page.locator("svg");
   await expect(jettisonPrFlowGraphSvg).toHaveCount(1);
   await expect(jettisonPrFlowGraphSvg).toBeVisible();
 
   // Test PR Flow Page - Workflow Execution
   const firstPrWorkflowDetails = page
-    .getByRole("link", { name: "See Details" })
+    .getByRole("link", { name: SEE_WORKFLOW_DETAILS_RE })
     .first();
   await expect(firstPrWorkflowDetails).toBeVisible();
   await firstPrWorkflowDetails.hover();
   await page.getByRole("button").click();
 
-  const viewPrWorkflowYamlLink = page.getByRole("link", { name: "View YAML" });
+  const viewPrWorkflowYamlLink = page.getByRole("link", { name: VIEW_YAML_RE });
   await expect(viewPrWorkflowYamlLink).toBeVisible();
   await expect(viewPrWorkflowYamlLink).toHaveAttribute("href", YAML_URL_RE);
 
   const viewPrWorkflowUiLink = page.getByRole("link", {
-    name: "View in Workflow UI",
+    name: VIEW_IN_WORKFLOW_UI_RE,
   });
   await expect(viewPrWorkflowUiLink).toBeVisible();
   await expect(viewPrWorkflowUiLink).toHaveAttribute(
@@ -96,7 +116,9 @@ test("test", async ({ page }) => {
   );
 
   await firstPrWorkflowDetails.click();
-  await expect(page.getByRole("link", { name: "Summary" })).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "summary", exact }),
+  ).toBeVisible();
 
-  await page.getByRole("link", { name: "PR Flow" }).click();
+  await page.getByRole("link", { name: PR_FLOW_RE }).click();
 });
