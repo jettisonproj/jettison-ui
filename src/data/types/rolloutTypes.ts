d11aab7834099dcbe5514b359ba9f265e33ae36a
Omar Soriano <osoriano2@gmail.com>
2026-07-26 15:12:34 -0700

    [rollout types] - add fields

--- src/data/types/rolloutTypes.ts
+++ src/data/types/rolloutTypes.ts
@@ -3,11 +3,56 @@ import { ResourceKinds } from "src/data/types/baseResourceTypes.ts";
 
 interface Rollout extends NamespacedResource {
   kind: typeof ResourceKinds.Rollout;
+  spec: RolloutSpec;
   status: RolloutStatus;
+}
+
+interface RolloutSpec {
+  strategy: RolloutStrategy;
+}
+
+interface RolloutStrategy {
+  canary: RolloutCanaryStrategy;
+}
+
+interface RolloutCanaryStrategy {
+  steps: RolloutCanaryStep[];
+}
+
+// SOT: https://pkg.go.dev/github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1#CanaryStep
+interface RolloutCanaryStep {
+  setCanaryScale?: SetCanaryScaleStep;
+  analysis?: RolloutAnalysisStep;
+  pause?: RolloutPauseStep;
+  setWeight?: number;
+}
+
+interface SetCanaryScaleStep {
+  weight?: number;
+  matchTrafficWeight?: boolean;
+}
+
+interface RolloutAnalysisStep {
+  templates: AnalysisTemplateRef[];
+  args: AnalysisRunArgument[];
+}
+
+interface AnalysisTemplateRef {
+  templateName: string;
+}
+
+interface AnalysisRunArgument {
+  name: string;
+  value: string;
+}
+
+interface RolloutPauseStep {
+  duration: string;
 }
 
 interface RolloutStatus {
   phase: RolloutPhase;
+  currentStepIndex: number;
 }
 
 // SOT: https://pkg.go.dev/github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1#RolloutPhase
@@ -24,4 +69,11 @@ const RolloutPhases = {
 type RolloutPhase = (typeof RolloutPhases)[keyof typeof RolloutPhases];
 
 export { RolloutPhases };
-export type { Rollout, RolloutPhase };
+export type {
+  Rollout,
+  RolloutAnalysisStep,
+  RolloutCanaryStep,
+  RolloutPauseStep,
+  RolloutPhase,
+  SetCanaryScaleStep,
+};
