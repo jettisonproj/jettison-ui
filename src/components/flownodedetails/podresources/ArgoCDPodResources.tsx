d2919c0d1e85656e8f938778f84b25148c170ee8
Omar <osoriano2@gmail.com>
2026-07-29 07:10:19 -0700

    move resource links to pod section (#71)
    * refactor resourcelinks
    
    * move links to pods

---
+++ src/components/flownodedetails/podresources/ArgoCDPodResources.tsx
@@ -0,0 +1,35 @@
+import type { JSX } from "react";
+
+import styles from "src/components/flownodedetails/podresources/ArgoCDPodResources.module.css";
+import { ArgoCDResourceLinks } from "src/components/flownodedetails/podresources/ArgoCDResourceLinks.tsx";
+import type {
+  Application,
+  ApplicationStatusResource,
+} from "src/data/types/applicationTypes.ts";
+import type { ArgoCDStep } from "src/data/types/flowTypes.ts";
+
+interface ArgoCDPodResourcesProps {
+  step: ArgoCDStep;
+  application: Application | undefined;
+  rolloutResource: ApplicationStatusResource | null;
+}
+function ArgoCDPodResources({
+  step,
+  application,
+  rolloutResource,
+}: ArgoCDPodResourcesProps): JSX.Element {
+  return (
+    <>
+      <h2 className={styles.sectionTitle}>
+        <span>Pod Resources</span>
+        <ArgoCDResourceLinks
+          step={step}
+          application={application}
+          rolloutResource={rolloutResource}
+        />
+      </h2>
+    </>
+  );
+}
+
+export { ArgoCDPodResources };
