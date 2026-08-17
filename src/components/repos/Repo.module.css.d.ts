f258c36efa1de078422e75565f3db8869b953871
Omar Soriano <osoriano2@gmail.com>
2026-08-16 18:37:17 -0700

    wip

--- src/components/repos/Repo.module.css.d.ts
+++ src/components/repos/Repo.module.css.d.ts
@@ -7,6 +7,10 @@ declare const styles: {
   readonly repoSubtitle: string;
   readonly repoSubtitleSkeleton: string;
   readonly repoTitle: string;
+  readonly repoBadges: string;
+  readonly repoHistoryStatusBadge: string;
+  readonly repoResourceStatusBadge: string;
+  readonly repoResourceStatusBadgeText: string;
   readonly repoWorkflowsEmpty: string;
 };
 
