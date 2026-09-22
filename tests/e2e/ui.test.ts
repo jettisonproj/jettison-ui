5fd3e46474fb2013bd4de3b3162e4bec9024de94
Omar Soriano <osoriano2@gmail.com>
2026-09-21 21:53:43 -0700

    fix test

--- tests/e2e/ui.test.ts
+++ tests/e2e/ui.test.ts
@@ -48,7 +48,9 @@ test("test", async ({ page }) => {
 
   // Test Push Flow Page
   await expect(
-    page.getByRole("heading", { name: `Home⧸Repos⧸${REPO_NAME}`, exact }),
+    page.getByRole("heading", {
+      name: new RegExp(`^Home⧸Repos⧸\\s*${REPO_NAME}\\s*`),
+    }),
   ).toBeVisible();
   await expect(page.getByRole("link", { name: /Push Flow$/ })).toBeVisible();
   const jettisonFlowPushGraphSvg = page.locator("svg");
